2e296b7b25b6bc12a0530d85205146fa5605bb2c
peppegiu <peppegiu2006@gmail.com>
2025-03-21 19:09:46 -0300

    fix divide function with different operation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
-    return a * b;
+    return a / b;
 }
 function operate(operator, a, b) {
     if (typeof a !== "number" && typeof b !== "number") {
