23ec9cdf5e33ee485dca02b55547a158677d56ef
peppegiu <peppegiu2006@gmail.com>
2025-03-21 18:11:46 -0300

    Make the numbers display on screen

--- script.js
+++ script.js
@@ -15,4 +15,31 @@ function operate(operator, a, b) {
 }
 
 
-let fisrtNumber, operator, secondNumber;
+let firstNumber, operator, secondNumber;
+let digits = document.querySelectorAll(".digit");
+const display = document.querySelector("#display");
+const clearDisplay = document.querySelector(".clear");
+let operators = document.querySelectorAll(".operator");
+
+
+operators.forEach((symbol) => {
+    symbol.addEventListener("click", () => {
+        if (display.value == undefined || display.value == null) {
+            firstNumber = 0;
+        }    
+        else {
+            firstNumber = display.value;
+            operator = symbol.value;
+        }
+        
+    })
+})
+
+
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        return display.value += digit.textContent;
+    })
+})
+
+clearDisplay.addEventListener("click", () => display.value = "");
