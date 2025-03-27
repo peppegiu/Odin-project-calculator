9f87de6a642c9c92b431b76b4899f071de82e4ed
peppegiu <peppegiu2006@gmail.com>
2025-03-27 10:35:54 -0300

    Clear the past buggy code and add a different itheration method

--- script.js
+++ script.js
@@ -32,48 +32,21 @@ function operate(operator, a, b) {
 }
 
 
-let firstNumber, operator, secondNumber;
+let firstNumber = 0
+let operator, secondNumber, current, result1;
 let digits = document.querySelectorAll(".digit");
 const display = document.querySelector("#display");
 const clearDisplay = document.querySelector(".clear");
 let operators = document.querySelectorAll(".operator");
 let equal = document.querySelector(".equal")
+let evaluated = false;
+let Selected, isSelected;
 
-operators.forEach((symbol) => {
-    symbol.addEventListener("click", () => {
-        if (display.value == undefined || display.value == null) {
-            firstNumber = 0;
-        }    
-        else {
-            firstNumber = display.value;
-            clear()
-            operator = symbol.value;
-        }
 
+for (let i = 0; i < digits.length; i++) {
+    digits[i].addEventListener("click", () => {
+        display.value += digits[i].textContent;
     })
-})
-
-
-digits.forEach((digit) => {
-    digit.addEventListener("click", () => {
-        return display.value += digit.textContent;
-    })
-})
-
-clearDisplay.addEventListener("click", clear);
-
-function clear() {
-    return display.value = "";
 }
 
 
-equal.addEventListener("click", () => {
-    if (display.value == null || display.value == undefined) {
-        secondNumber = firstNumber;
-    }
-    else {
-        secondNumber = display.value;
-    }
-    let result = operate(operator, firstNumber, secondNumber);
-    display.value = result
-})
